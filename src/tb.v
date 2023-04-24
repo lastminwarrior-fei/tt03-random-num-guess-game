b7b7a67a107ef942f866800cdb4a453310dc7b22
lastminwarrior-fei <131673089+lastminwarrior-fei@users.noreply.github.com>
2023-04-24 10:18:26 -0400

    update tb.v

--- src/tb.v
+++ src/tb.v
@@ -1,38 +1,26 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
-*/
-
 module tb (
-    // testbench is controlled by test.py
-    input clk,
-    input rst,
-    output [6:0] segments
-   );
+    input wire clk,
+    input wire rst,
+    input wire [5:0] guess,
+    output wire [5:0] result    
+    );
 
-    // this part dumps the trace to a vcd file that can be viewed with GTKWave
     initial begin
         $dumpfile ("tb.vcd");
         $dumpvars (0, tb);
         #1;
     end
 
-    // wire up the inputs and outputs
-    wire [7:0] inputs = {6'b0, rst, clk};
+    wire [7:0] inputs = {guess[5:0], rst, clk};
     wire [7:0] outputs;
-    assign segments = outputs[6:0];
+    assign result = outputs[5:0]; 
 
-    // instantiate the DUT
-    seven_segment_seconds seven_segment_seconds(
-        `ifdef GL_TEST
-            .vccd1( 1'b1),
-            .vssd1( 1'b0),
-        `endif
-        .io_in  (inputs),
+    guess_game guess_game (
+        .io_in (inputs),
         .io_out (outputs)
-        );
+    );
 
 endmodule
