d66f8cc8e4258bdbe076c06e99e4fab14a13dff7
lastminwarrior-fei <131673089+lastminwarrior-fei@users.noreply.github.com>
2023-04-24 12:06:51 -0400

    update test.py

--- src/test.py
+++ src/test.py
@@ -5,3 +5,11 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     dut._log.info("start")
+    clock = Clock(dut.clk, 1, units="ms")
+    cocotb.start_soon(clock.start())
+    
+    dut.rst.value = 1
+    await ClockCycles(dut.CLK, 5)
+    dut.rst = 0
+    assert dut.result.value == 0b000000
+    dut._log.info("test0 passed")
