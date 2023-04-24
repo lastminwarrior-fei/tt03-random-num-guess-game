fafb8e08a777b793b759e5e8d9804e615a7e9faf
lastminwarrior-fei <131673089+lastminwarrior-fei@users.noreply.github.com>
2023-04-24 12:27:21 -0400

    update test.py

--- src/test.py
+++ src/test.py
@@ -9,7 +9,32 @@ async def test_my_design(dut):
     cocotb.start_soon(clock.start())
     
     dut.rst.value = 1
-    await ClockCycles(dut.clk, 5)
+    await ClockCycles(dut.clk, 1)
     dut.rst = 0
     assert dut.result.value == 0b000000
-    dut._log.info("test0 passed")
+    dut._log.info("reset test passed")
+    
+    dut.guess.value = 0b000000
+    dut._log.info("guess 1: ", dut.guess.value)
+    await ClockCycles(dut.clk, 5)
+    dut._log.info("result 1: ", dut.result.value) 
+    
+    dut.guess.value = 0b000001
+    dut._log.info("guess 2: ", dut.guess.value)
+    await ClockCycles(dut.clk, 1)
+    dut._log.info("result 2: ", dut.result.value) 
+    
+    dut.guess.value = 0b000011
+    dut._log.info("guess 3: ", dut.guess.value)
+    await ClockCycles(dut.clk, 1)
+    dut._log.info("result 3: ", dut.result.value) 
+    
+    dut.guess.value = 0b000000
+    dut._log.info("guess 4: ", dut.guess.value)
+    await ClockCycles(dut.clk, 1)
+    dut._log.info("result 4: ", dut.result.value) 
+    
+    dut.guess.value = 0b000000
+    dut._log.info("guess 5: ", dut.guess.value)
+    await ClockCycles(dut.clk, 1)
+    dut._log.info("result 5: ", dut.result.value) 
