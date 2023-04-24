0bb9da47ba7916ebe953cfe0ad59a0710c2d5b39
lastminwarrior-fei <131673089+lastminwarrior-fei@users.noreply.github.com>
2023-04-24 12:39:54 -0400

    update test.py

--- src/test.py
+++ src/test.py
@@ -9,7 +9,7 @@ async def test_my_design(dut):
     cocotb.start_soon(clock.start())
     
     dut.rst.value = 1
-    await ClockCycles(dut.clk, 2)
+    await ClockCycles(dut.clk, 5)
     dut.rst = 0
     assert dut.result.value == 0b000000
     dut._log.info("reset test passed")
