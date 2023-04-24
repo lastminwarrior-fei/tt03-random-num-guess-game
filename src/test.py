8b36069441ee256f13ec263dab4acc5bb9ddc109
lastminwarrior-fei <131673089+lastminwarrior-fei@users.noreply.github.com>
2023-04-24 12:57:04 -0400

    update test.py

--- src/test.py
+++ src/test.py
@@ -9,32 +9,32 @@ async def test_my_design(dut):
     cocotb.start_soon(clock.start())
     
     dut.rst.value = 1
-    await ClockCycles(dut.clk, 5)
+    await ClockCycles(dut.clk, 10)
     dut.rst = 0
     assert dut.result.value == 0b000000
     dut._log.info("reset test passed")
     
     dut.guess.value = 0b000000
-    dut._log.info("guess 1: ", str(dut.guess.value))
-    await ClockCycles(dut.clk, 1)
-    dut._log.info("result 1: ", str(dut.result.value)) 
+    dut._log.info("guess 1: " + str(dut.guess.value))
+    await ClockCycles(dut.clk, 2)
+    dut._log.info("result 1: " + str(dut.result.value)) 
     
     dut.guess.value = 0b000001
-    dut._log.info("guess 2: ", str(dut.guess.value))
-    await ClockCycles(dut.clk, 1)
-    dut._log.info("result 2: ", str(dut.result.value)) 
+    dut._log.info("guess 2: " + str(dut.guess.value))
+    await ClockCycles(dut.clk, 2)
+    dut._log.info("result 2: " + str(dut.result.value)) 
     
     dut.guess.value = 0b000011
-    dut._log.info("guess 3: ", str(dut.guess.value))
-    await ClockCycles(dut.clk, 1)
-    dut._log.info("result 3: ", str(dut.result.value)) 
+    dut._log.info("guess 3: " + str(dut.guess.value))
+    await ClockCycles(dut.clk, 2)
+    dut._log.info("result 3: " + str(dut.result.value)) 
     
     dut.guess.value = 0b000000
-    dut._log.info("guess 4: ", str(dut.guess.value))
-    await ClockCycles(dut.clk, 1)
-    dut._log.info("result 4: ", str(dut.result.value)) 
+    dut._log.info("guess 4: " + str(dut.guess.value))
+    await ClockCycles(dut.clk, 2)
+    dut._log.info("result 4: " + str(dut.result.value)) 
     
     dut.guess.value = 0b000000
-    dut._log.info("guess 5: ", str(dut.guess.value))
-    await ClockCycles(dut.clk, 1)
-    dut._log.info("result 5: ", str(dut.result.value)) 
+    dut._log.info("guess 5: " + str(dut.guess.value))
+    await ClockCycles(dut.clk, 2)
+    dut._log.info("result 5: " + str(dut.result.value)) 
