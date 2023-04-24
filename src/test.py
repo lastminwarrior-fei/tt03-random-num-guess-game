6313fe66f1d8562100a64954d5dd44163ef2431b
lastminwarrior-fei <131673089+lastminwarrior-fei@users.noreply.github.com>
2023-04-24 12:46:14 -0400

    update test.py

--- src/test.py
+++ src/test.py
@@ -15,26 +15,26 @@ async def test_my_design(dut):
     dut._log.info("reset test passed")
     
     dut.guess.value = 0b000000
-    dut._log.info("guess 1: ", dut.guess.value)
+    dut._log.info("guess 1: ", str(dut.guess.value))
     await ClockCycles(dut.clk, 1)
-    dut._log.info("result 1: ", dut.result.value) 
+    dut._log.info("result 1: ", str(dut.result.value)) 
     
     dut.guess.value = 0b000001
-    dut._log.info("guess 2: ", dut.guess.value)
+    dut._log.info("guess 2: ", str(dut.guess.value))
     await ClockCycles(dut.clk, 1)
-    dut._log.info("result 2: ", dut.result.value) 
+    dut._log.info("result 2: ", str(dut.result.value)) 
     
     dut.guess.value = 0b000011
-    dut._log.info("guess 3: ", dut.guess.value)
+    dut._log.info("guess 3: ", str(dut.guess.value))
     await ClockCycles(dut.clk, 1)
-    dut._log.info("result 3: ", dut.result.value) 
+    dut._log.info("result 3: ", str(dut.result.value)) 
     
     dut.guess.value = 0b000000
-    dut._log.info("guess 4: ", dut.guess.value)
+    dut._log.info("guess 4: ", str(dut.guess.value))
     await ClockCycles(dut.clk, 1)
-    dut._log.info("result 4: ", dut.result.value) 
+    dut._log.info("result 4: ", str(dut.result.value)) 
     
     dut.guess.value = 0b000000
-    dut._log.info("guess 5: ", dut.guess.value)
+    dut._log.info("guess 5: ", str(dut.guess.value))
     await ClockCycles(dut.clk, 1)
-    dut._log.info("result 5: ", dut.result.value) 
+    dut._log.info("result 5: ", str(dut.result.value)) 
